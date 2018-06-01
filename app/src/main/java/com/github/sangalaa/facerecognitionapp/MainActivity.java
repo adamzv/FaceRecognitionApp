7a10543a341a5e585a14eb3353b7bf88deda298b
Adam Zverka <adamzverka@hotmail.com>
2018-06-01 21:22:27 +0200

    Added CameraKit

--- app/src/main/java/com/github/sangalaa/facerecognitionapp/MainActivity.java
+++ app/src/main/java/com/github/sangalaa/facerecognitionapp/MainActivity.java
@@ -1,34 +1,91 @@
 package com.github.sangalaa.facerecognitionapp;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.wonderkiln.camerakit.CameraKit;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraProperties;
+import com.wonderkiln.camerakit.CameraView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private CameraView cameraView;
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.take_picture:
+
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpCameraView();
+        Button button = (Button) findViewById(R.id.take_picture);
+        if (button != null) {
+            button.setOnClickListener(onClickListener);
+        }
+
+        cameraView.setMethod(CameraKit.Constants.METHOD_STILL);
+        cameraView.setCropOutput(false);
+        cameraView.setPinchToZoom(false);
+        cameraView.setZoom(0);
+
     }
 
-    /**
-     * Checks if this device has a front facing camera
-     **/
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)) {
-            return true;
-        } else {
-            return false;
-        }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
+
     }
 
-    public static Camera getCameraInstance() {
-        Camera camera = null;
-        try {
-            camera = Camera.open();
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    public void setUpCameraView() {
+        cameraView = (CameraView) findViewById(R.id.camera_view);
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                Log.d(TAG, "onImage");
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
     }
 }
