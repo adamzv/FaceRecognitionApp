af67c4a2e51c3ca207740f842b8f4a1af6faca56
Adam Zverka <adamzverka@hotmail.com>
2018-06-07 22:56:59 +0200

    Added AsyncTask for calling Visual Recognition Service

--- app/src/main/java/com/github/sangalaa/facerecognitionapp/MainActivity.java
+++ app/src/main/java/com/github/sangalaa/facerecognitionapp/MainActivity.java
@@ -1,24 +1,31 @@
 package com.github.sangalaa.facerecognitionapp;
 
-import android.content.Intent;
+import android.content.Loader;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.wonderkiln.camerakit.CameraKit;
+import com.ibm.watson.developer_cloud.http.ServiceCallback;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
 import com.wonderkiln.camerakit.CameraKitError;
 import com.wonderkiln.camerakit.CameraKitEvent;
 import com.wonderkiln.camerakit.CameraKitEventListener;
 import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraKitVideo;
-import com.wonderkiln.camerakit.CameraProperties;
 import com.wonderkiln.camerakit.CameraView;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private FloatingActionButton takePictureButton;
     private FloatingActionButton backButton;
+
+    private VisualRecognition visualRecognition;
+    private DetectFacesOptions detectFacesOptions;
 
     private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
@@ -61,12 +71,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         imageView = (ImageView) findViewById(R.id.image);
+        imageView.setVisibility(View.VISIBLE);
 
-        cameraView.setMethod(CameraKit.Constants.METHOD_STANDARD);
-        cameraView.setZoom(0);
+        /*
+         * Create credentials.xml resource file to store
+         * Visual Recognition apikey and API version
+         * <resources>
+         *  <string name="visual_recognition_apikey">{your_apikey}</string>
+         *  <string name="visual_recognition_version">{vesion   }</string>
+         * </resources>
+         */
+        IamOptions options = new IamOptions.Builder()
+                .apiKey(getString(R.string.visual_recognition_apikey))
+                .build();
 
+        visualRecognition = new VisualRecognition(getString(R.string.visual_recognition_version), options);
     }
-
 
     @Override
     protected void onPause() {
@@ -86,27 +106,28 @@ public class MainActivity extends AppCompatActivity {
         cameraView.addCameraKitListener(new CameraKitEventListener() {
             @Override
             public void onEvent(CameraKitEvent cameraKitEvent) {
-
             }
 
             @Override
             public void onError(CameraKitError cameraKitError) {
-
             }
 
             @Override
             public void onImage(CameraKitImage cameraKitImage) {
-                Log.d(TAG, "onImage");
-                Toast.makeText(MainActivity.this, "onImage", Toast.LENGTH_LONG).show();
                 Bitmap result = cameraKitImage.getBitmap();
 
                 displayImage(result);
 
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                result.compress(Bitmap.CompressFormat.PNG, 0, byteArrayOutputStream);
+                byte[] resultByteArray = byteArrayOutputStream.toByteArray();
+                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(resultByteArray);
+
+                new DetectFacesTask().execute(byteArrayInputStream);
             }
 
             @Override
             public void onVideo(CameraKitVideo cameraKitVideo) {
-
             }
         });
     }
@@ -132,4 +153,20 @@ public class MainActivity extends AppCompatActivity {
         takePictureButton.setVisibility(View.VISIBLE);
         backButton.setVisibility(View.INVISIBLE);
     }
+
+    private class DetectFacesTask extends AsyncTask<InputStream, Void, DetectedFaces> {
+
+        @Override
+        protected DetectedFaces doInBackground(InputStream... inputStreams) {
+            detectFacesOptions = new DetectFacesOptions.Builder().imagesFile(inputStreams[0]).build();
+
+            return visualRecognition.detectFaces(detectFacesOptions).execute();
+        }
+
+        @Override
+        protected void onPostExecute(DetectedFaces detectedFaces) {
+            Log.d("IBM", detectedFaces.toString());
+        }
+    }
+
 }
